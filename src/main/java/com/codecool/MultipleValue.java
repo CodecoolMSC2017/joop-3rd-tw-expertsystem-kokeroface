71f585517c9b343abe807397ecb74a9092198db1
daviddude <uszkai.d@gmail.com>
2018-02-06 13:37:23 +0100

    multiple value variables declared

--- src/main/java/com/codecool/MultipleValue.java
+++ src/main/java/com/codecool/MultipleValue.java
@@ -5,8 +5,11 @@ import java.util.List;
 public class MultipleValue extends Value {
 
     List<String> params;
+    boolean selectionType;
 
     public MultipleValue(List<String> params, boolean selectionType) {
+        this.params = params;
+        this.selectionType = selectionType;
 
     }
 }
