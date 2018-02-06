3b29c09ead2166b8335cbca45a7ef32a9b85f253
daviddude <uszkai.d@gmail.com>
2018-02-06 13:31:21 +0100

    multiple value argument added

--- src/main/java/com/codecool/MultipleValue.java
+++ src/main/java/com/codecool/MultipleValue.java
@@ -6,7 +6,7 @@ public class MultipleValue extends Value {
 
     List<String> params;
 
-    public MultipleValue(List<String> params) {
+    public MultipleValue(List<String> params, boolean selectionType) {
 
     }
 }
