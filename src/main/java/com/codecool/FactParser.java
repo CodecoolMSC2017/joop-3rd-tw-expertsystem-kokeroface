11b7b80bc0effc993ccda01424114d2259b9812e
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 10:34:01 +0100

    FactParser theoretically working

--- src/main/java/com/codecool/FactParser.java
+++ src/main/java/com/codecool/FactParser.java
@@ -1,7 +1,46 @@
 package com.codecool;
 
-public class FactParser {
-    public FactRepository getFactRepository() {
-        return null;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+public class FactParser extends XMLParser {
+
+    String id,description,evalid;
+    boolean value;
+    Fact fact;
+    Document doc;
+
+
+    public FactRepository getFactRepository() throws Exception {
+        loadXmlDocument("facts.xml");
+        NodeList nList = doc.getElementsByTagName("Fact");
+        FactRepository factRepository = new FactRepository();
+        for (int temp = 0; temp < nList.getLength() ; temp++) {
+
+            Node nNode = nList.item(temp);
+            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) nNode;
+                id = eElement.getAttribute("id");
+                description = eElement.getElementsByTagName("Description").item(0).getAttributes().getNamedItem("value").getNodeValue();
+                fact = new Fact(id, description);
+                NodeList evals = eElement.getElementsByTagName("Eval");
+                for (int i = 0; i < evals.getLength() ; i++) {
+                    Node mNode = evals.item(i);
+                    if(mNode.getNodeType() == Node.ELEMENT_NODE){
+                        Element eval = (Element)mNode;
+                        String evalId = eval.getAttribute("id");
+                        value = Boolean.parseBoolean(eval.getTextContent().trim());
+
+                        fact.setFactValueById(evalId, value);
+                    }
+                }
+                factRepository.addFact(fact);
+            }
+
+        }
+
+        return factRepository;
     }
 }
