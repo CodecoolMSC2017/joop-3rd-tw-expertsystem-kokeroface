a6bd6bd4ec26062346ba233e7ad00166d86c25bf
daviddude <uszkai.d@gmail.com>
2018-02-06 13:32:12 +0100

    value method added

--- src/main/java/com/codecool/Value.java
+++ src/main/java/com/codecool/Value.java
@@ -3,8 +3,13 @@ package com.codecool;
 import java.util.List;
 
 public abstract class Value {
+
     public List<String> getInputPattern() {
 
         return null;
+    }
+
+    public boolean getSelectionType(){
+        return true;
     }
 }
