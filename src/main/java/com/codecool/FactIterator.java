dee165deaa7ca42b236a836baa76a9d80517f9c8
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 10:33:18 +0100

    FactIterator implemented

--- src/main/java/com/codecool/FactIterator.java
+++ src/main/java/com/codecool/FactIterator.java
@@ -1,16 +1,29 @@
 package com.codecool;
 
 import java.util.Iterator;
+import java.util.List;
 
-public class FactIterator implements Iterator<Fact> {
+public class FactIterator implements Iterator {
+
+    private int i;
+    private List<Fact> list;
+
+    public FactIterator(List<Fact> list) {
+        this.list = list;
+    }
 
     @Override
     public boolean hasNext() {
+        if (i + 1 < list.size()) {
+            return true;
+        }
         return false;
     }
 
     @Override
     public Fact next() {
-        return null;
+        Fact fact = list.get(i);
+        i++;
+        return fact;
     }
 }
