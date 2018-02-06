f3c17291e44df84711883ba61c8c736dede9f2b2
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 12:39:42 +0100

    Main printing the contents of rules xml

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -1,9 +1,17 @@
 package com.codecool;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
-    public static void main(String[] args) {
-        FactParser facts = new FactParser();
-        RuleParser rules = new RuleParser();
-        ESProvider esp = new ESProvider(facts, rules);
+    public static void main(String[] args) throws Exception {
+        FactParser fp = new FactParser();
+        FactRepository repo = fp.getFactRepository();
+        List<Fact> facts = repo.getFactList();
+
+
+        for (Fact fact : facts) {
+            System.out.println(fact.id+"\t\t\t\t\t\t\t\t"+fact.description);
+        }
     }
 }
