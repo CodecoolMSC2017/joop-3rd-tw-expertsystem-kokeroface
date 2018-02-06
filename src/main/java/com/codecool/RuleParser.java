1e98467d56bfca87c0d232b43cb981a3dbd1b0f2
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 18:44:34 +0100

    RuleParser theoretically working

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -5,37 +5,53 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
+import java.util.Arrays;
+
 public class RuleParser extends XMLParser {
 
     public RuleRepository getRuleRepository() throws Exception {
         loadXmlDocument("rules.xml");
-        Document doc = getDoc();
-        NodeList nList = doc.getElementsByTagName("Rule");
         RuleRepository ruleRepository = new RuleRepository();
-        for (int temp = 0; temp < nList.getLength() ; temp++){
-
-            Node nNode = nList.item(temp);
-            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                Element eElement = (Element) nNode;
+        Document doc = getDoc();
+        NodeList nlist = doc.getElementsByTagName("Rule");
+        for(int i = 0; i < nlist.getLength(); i++){
+            Node node = nlist.item(i);
+            if(node.getNodeType() == Node.ELEMENT_NODE) {
+                Element eElement = (Element) node;
                 String id = eElement.getAttribute("id");
+
                 Answer answer = new Answer();
-                answer.addValue(new SingleValue("true", true));
-                answer.addValue(new SingleValue("false", false));
-                Question question = new Question(id, eElement.getElementsByTagName("Question").item(0).getTextContent(), answer);
-                /*for (int temp2 = 0; temp2 < answer.getLength() ; temp2++) {
-                    Node nNode2 = answer.item(temp2);
-                    if (nNode2.getNodeType() == Node.ELEMENT_NODE) {
-                        NodeList selection = doc.getElementsByTagName("Selection");
-                        for (int temp3 = 0; temp3 < nList.getLength(); temp3++) {
-                            Node nNode3 = nList.item(temp3);
-                            Element eElement2 = (Element) nNode;
-                            String value = eElement2.getElementsByTagName("value").item(0).getTextContent();
+
+                Node n = eElement.getElementsByTagName("Answer").item(0);
+                for(int k = 0; k < 2; k++){
+                    boolean bool = false;
+                    Node selection = ((Element)n).getElementsByTagName("Selection").item(k);
+                    Element e = (Element)selection;
+                    String value;
+                    if(e.getElementsByTagName("MultipleValue").getLength() != 0) {
+                        value = e.getElementsByTagName("MultipleValue").item(0).getAttributes().getNamedItem("value").getNodeValue();
+
+                        if(k == 0) {
+                            bool = true;
                         }
+
+                        answer.addValue(new MultipleValue(Arrays.asList(value.split(",")),bool));
+                    } else {
+                        value = e.getElementsByTagName("SingleValue").item(0).getAttributes().getNamedItem("value").getNodeValue();
+
+                        if(k == 0) {
+                            bool = true;
+                        }
+
+                        answer.addValue(new SingleValue(value,bool));
                     }
-                }*/
+
+                }
+                Question question = new Question(id,eElement.getElementsByTagName("Question").item(0).getTextContent(),answer);
                 ruleRepository.addQuestion(question);
             }
         }
         return ruleRepository;
     }
 }
+
