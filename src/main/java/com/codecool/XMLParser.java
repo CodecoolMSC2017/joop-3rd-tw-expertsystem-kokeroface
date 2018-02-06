cf420d6243c0e416326271e3085cddebd559b98a
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 10:34:20 +0100

    XMLParser theoretically working

--- src/main/java/com/codecool/XMLParser.java
+++ src/main/java/com/codecool/XMLParser.java
@@ -1,9 +1,6 @@
 package com.codecool;
 
 import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,40 +12,18 @@ public abstract class XMLParser {
 
     private List<Fact> facts = new ArrayList<>();
     private List<Question> questions = new ArrayList<>();
+    private Document doc;
 
-    public void loadXmlDocument(String xmlPath) throws Exception {
-        Answer answer = new Answer();
+    protected void loadXmlDocument(String xmlPath) throws Exception {
         try {
-            File fXmlFile = new File(xmlPath);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc = dBuilder.parse(fXmlFile);
-
+            File input = new File(xmlPath);
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            doc = documentBuilder.parse(input);
             doc.getDocumentElement().normalize();
-            try {
-                NodeList nList = doc.getElementsByTagName("Fact");
-                for (int temp = 0; temp < nList.getLength(); temp++) {
-                    Node nNode = nList.item(temp);
-                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element eElement = (Element) nNode;
-                        Fact fact = new Fact(eElement.getElementsByTagName("id").item(0).getTextContent(),
-                                eElement.getElementsByTagName("value").item(0).getTextContent());
-                        facts.add(fact);
-                    }
-                }
-            } catch (Exception e) {
-                NodeList nList = doc.getElementsByTagName("Rule");
-                for (int temp = 0; temp < nList.getLength(); temp++) {
-                    Node nNode = nList.item(temp);
-                    if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-                        Element eElement = (Element) nNode;
-                        Question question = new Question(eElement.getElementsByTagName("id").item(0).getTextContent(), answer);
-                        questions.add(question);
-                    }
-                }
-            }
-        } catch(Exception e) {
-            e.printStackTrace();
+
+        } catch (Exception e) {
+            throw new Exception();
         }
     }
 }
