140fe495ab7e1c219e4531bc8afccd651ca9de37
daviddude <uszkai.d@gmail.com>
2018-02-06 13:37:05 +0100

    single value variables declared

--- src/main/java/com/codecool/SingleValue.java
+++ src/main/java/com/codecool/SingleValue.java
@@ -1,7 +1,12 @@
 package com.codecool;
 
 public class SingleValue extends Value {
+    String string;
+    boolean selectionType;
+
     public SingleValue(String string,boolean selectionType) {
+        this.string = string;
+        this.selectionType = selectionType;
 
     }
 }
