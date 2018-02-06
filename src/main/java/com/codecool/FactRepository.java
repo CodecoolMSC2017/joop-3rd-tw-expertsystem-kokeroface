3dd3e73680400e6c5ea3fac159a500db6bc73c54
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 12:39:21 +0100

    FactRepo working

--- src/main/java/com/codecool/FactRepository.java
+++ src/main/java/com/codecool/FactRepository.java
@@ -1,10 +1,19 @@
 package com.codecool;
 
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
 public class FactRepository {
-    public void addFact(Fact fact) {
 
+    private List<Fact> facts = new ArrayList<>();
+
+    public void addFact(Fact fact) {
+        facts.add(fact);
+    }
+
+    public List<Fact> getFactList() {
+        return facts;
     }
 
     public Iterator<Fact> getIterator() {
