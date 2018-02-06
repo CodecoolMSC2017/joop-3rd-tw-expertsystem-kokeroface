4d0c6ca2c366b52bc5cb217b77f00187a3517e63
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-06 09:36:07 +0100

    Main instantiation

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -2,6 +2,8 @@ package com.codecool;
 
 public class Main {
     public static void main(String[] args) {
-
+        FactParser facts = new FactParser();
+        RuleParser rules = new RuleParser();
+        ESProvider esp = new ESProvider(facts, rules);
     }
 }
