05c540b8012f82ba6dbfa7b78af3565b3281e946
daviddude <uszkai.d@gmail.com>
2018-02-06 14:15:17 +0100

    RuleParser started implementation

--- src/main/java/com/codecool/RuleParser.java
+++ src/main/java/com/codecool/RuleParser.java
@@ -1,8 +1,49 @@
 package com.codecool;
 
-public class RuleParser {
-    public RuleRepository getRuleRepository() {
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
-        return null;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+
+
+public class RuleParser extends XMLParser {
+    private RuleRepository repo = new RuleRepository();
+
+    public RuleRepository getRuleRepository() {
+        return repo;
+    }
+
+
+    public void loadXmlDocument(String xmlPath){
+        Document doc = null;
+        try {
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dbBuilder = dbFactory.newDocumentBuilder();
+            doc = dbBuilder.parse(new File(xmlPath));
+        }
+        catch (Exception o) {
+            o.printStackTrace();
+        }
+        String id;
+        String question;
+        Question quest;
+        Answer answer = new Answer();
+        Value value = new SingleValue("true",true);
+        answer.addValue(value);
+        answer.addValue(new SingleValue("false",false));
+        NodeList nodes = doc.getElementsByTagName("Rule");
+        for (int i = 0; i < nodes.getLength() ; i++) {
+            Element element;
+            element =  (Element) nodes.item(i);
+            id = element.getAttribute("id");
+            NodeList nodeList = element.getChildNodes();
+            question = nodeList.item(1).getTextContent();
+
+            quest = new Question(id, question, answer );
+            repo.addQuestion(quest);
+        }
     }
 }
