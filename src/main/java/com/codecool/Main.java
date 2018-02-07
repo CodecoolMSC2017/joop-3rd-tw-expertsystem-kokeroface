5ddc54bb947f4ca772c16009ba8ffcede9b5a8c7
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-07 10:26:16 +0100

    Main now only has ESProvider instance

--- src/main/java/com/codecool/Main.java
+++ src/main/java/com/codecool/Main.java
@@ -5,13 +5,5 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws Exception {
         ESProvider esp = new ESProvider(new FactParser(), new RuleParser());
-        FactRepository factrepo = esp.getFactRepository();
-        RuleRepository rulerepo = esp.getRuleRepository();
-        Iterator<Fact> fit = factrepo.getIterator();
-        Iterator<Question> qit = rulerepo.getIterator();
-
-        while (qit.hasNext()) {
-            esp.getAnswerByQuestion(qit.next().id);
-        }
     }
 }
