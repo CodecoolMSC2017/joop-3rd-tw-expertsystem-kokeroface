7ae138773d372b63c89c70cb1e91a8c556ea252c
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-07 19:34:50 +0100

    ESProvider every method implemented

--- src/main/java/com/codecool/ESProvider.java
+++ src/main/java/com/codecool/ESProvider.java
@@ -1,14 +1,14 @@
 package com.codecool;
 
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Scanner;
+import java.util.*;
 
 public class ESProvider {
 
     private FactRepository factRepository;
     private RuleRepository ruleRepository;
-    private HashMap<String, Boolean> answers = new HashMap<>();
+    private Map<String, Boolean> answers = new HashMap<>();
+    private Map<String, Integer> games = new HashMap<>();
+    private String game = "";
 
     ESProvider(FactParser fact, RuleParser rule) throws Exception {
         this.factRepository = fact.getFactRepository();
@@ -17,11 +17,30 @@ public class ESProvider {
 
     public void collectAnswers() {
         Iterator<Question> questionIterator = ruleRepository.getIterator();
-        int counter = 0;
+        Iterator<Fact> factIterator = factRepository.getIterator();
         while (questionIterator.hasNext()) {
             Question question = questionIterator.next();
-            this.answers.put(question.getId(), answers.get(counter));
-            counter++;
+            boolean answer = getAnswerByQuestion(question.id);
+            this.answers.put(question.getId(), answer);
+        }
+        while (factIterator.hasNext()) {
+            Map<String, Boolean> questions = new HashMap<>();
+            Fact fact = factIterator.next();
+            Set<String> idSet = fact.getIdSet();
+            List<String> idList = new ArrayList<>(idSet);
+            for (int i = 0; i < idList.size(); i++) {
+                questions.put(idList.get(i), fact.getValueById(idList.get(i)));
+            }
+            int counter = 0;
+            for (Map.Entry<String, Boolean> bool : questions.entrySet()) {
+                for (Map.Entry<String, Boolean> entry : answers.entrySet()) {
+                    if (bool.getValue() == entry.getValue() && bool.getKey().equals(entry.getKey())) {
+                        counter++;
+                    }
+                }
+                games.put(fact.getDescription(),counter);
+            }
+            counter = 0;
         }
     }
 
@@ -29,10 +48,21 @@ public class ESProvider {
 
         Scanner userInput = new Scanner(System.in);
         Iterator<Question> iterator = ruleRepository.getIterator();
+
         while (iterator.hasNext()) {
             Question question = iterator.next();
+            List<Value> values = question.getAnswer().getValues();
             if (questionId.equals(question.getId())) {
-                System.out.println(question.getQuestion());
+                if (values.get(0) instanceof SingleValue) {
+                    System.out.println(question.getQuestion());
+                } else {
+                    System.out.println(question.getQuestion());
+                    System.out.println("\nChoices:");
+                    for (int j = 0; j < values.get(0).param.size(); j++) {
+                        System.out.println(values.get(0).param.get(j));
+                        System.out.println(values.get(1).param.get(j));
+                    }
+                }
                 while (true) {
                     try {
                         String answer = userInput.nextLine();
@@ -48,7 +78,13 @@ public class ESProvider {
     }
 
     public String evaluate() {
-        return null;
+        int maxValueInMap = (Collections.max(games.values()));
+        for (Map.Entry<String, Integer> item : games.entrySet()) {
+            if (item.getValue() == maxValueInMap) {
+                game += item.getKey()+", ";
+            }
+        }
+        return game.substring(0, game.length() - 2);
     }
 
     public FactRepository getFactRepository() {
