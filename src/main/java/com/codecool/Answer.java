63fbb054d97ee89fe37897b92c5959a64a6b68fd
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-07 14:45:36 +0100

    Answer evaluateAnswerByInput implemented, seems to be working the way intended.

--- src/main/java/com/codecool/Answer.java
+++ src/main/java/com/codecool/Answer.java
@@ -7,16 +7,35 @@ public class Answer {
 
     @SuppressWarnings("MismatchedQueryAndUpdateOfCollection")
     private List<Value> values = new ArrayList<>();
+    private int i;
+    private String possibleAnswer = "";
 
     public boolean evaluateAnswerByInput(String input) throws Exception {
+        if (values.get(i) instanceof SingleValue) {
+            if (input.equals(values.get(i).param.get(0).toString())) {
+                return values.get(i).selectionType;
+            } else if (input.equals(values.get(i + 1).param.get(0).toString())) {
+                return values.get(i + 1).selectionType;
+            }
+            throw new Exception("Please enter either " + values.get(i).param.get(0) + " or " + values.get(i + 1).param.get(0));
 
-        input = input.toLowerCase();
-        if (input.equals("yes") ) {
-            return true;
-        } else if (input.equals("no")) {
-            return false;
+        } else {
+            for (int j = 0; j < values.get(i).param.size(); j++) {
+                possibleAnswer += values.get(i).param.get(j)+"\n";
+                possibleAnswer += values.get(i+1).param.get(j)+"\n";
+            }
+
+            for (int j = 0; j < values.get(i).param.size(); j++) {
+                if (input.equals(values.get(i).param.get(j).toString())) {
+                    System.out.println(values.get(i).selectionType);
+                    return values.get(i).selectionType;
+                } else if (input.equals(values.get(i+1).param.get(j).toString())) {
+                    System.out.println(values.get(i+1).selectionType);
+                    return values.get(i + 1).selectionType;
+                }
+            }
+            throw new Exception("Please enter one of the following:\n"+possibleAnswer);
         }
-        throw new Exception("Wrong input");
     }
 
     public void addValue(Value value) {
