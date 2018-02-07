7431180f1f62e6fd36e00db53cab20aa8dfb8492
Csanád Hegedűs <hegedus.csanad96@gmail.com>
2018-02-07 14:48:06 +0100

    SingleValue inheritance modified to match xml reading

--- src/main/java/com/codecool/SingleValue.java
+++ src/main/java/com/codecool/SingleValue.java
@@ -1,8 +1,11 @@
 package com.codecool;
 
+import java.util.List;
+
 class SingleValue extends Value {
 
     SingleValue(String param, boolean selectionType) {
+
         super(param,selectionType);
     }
 }
