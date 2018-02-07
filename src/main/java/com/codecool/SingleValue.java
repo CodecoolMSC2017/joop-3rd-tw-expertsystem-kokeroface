ae1a518f892e6e6fd0d11ac0e1770653156ce0c8
daviddude <uszkai.d@gmail.com>
2018-02-07 10:23:44 +0100

    SingleValue implementation again

--- src/main/java/com/codecool/SingleValue.java
+++ src/main/java/com/codecool/SingleValue.java
@@ -2,11 +2,7 @@ package com.codecool;
 
 class SingleValue extends Value {
 
-    private String string;
-    private boolean selectionType;
-
-    SingleValue(String string, boolean selectionType) {
-        this.string = string;
-        this.selectionType = selectionType;
+    SingleValue(String param, boolean selectionType) {
+        super(param,selectionType);
     }
 }
