0a6751136f95b88666636bf905e26e0a62a2ae8d
zsarna2 <zsarna2@gmail.com>
2018-02-08 11:37:09 +0100

    FactParser Test implemented

--- src/test/java/com/codecool/FactParserTest.java
+++ src/test/java/com/codecool/FactParserTest.java
@@ -1,16 +1,22 @@
 package com.codecool;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FactParserTest {
 
-    @BeforeEach
-    void setUp() {
-    }
+    FactParser factParserTest = new FactParser();
 
     @Test
     void getFactRepository() throws Exception {
+        FactRepository test = factParserTest.getFactRepository();
+        Fact mortal_kombat_komplete_edition = test.getIterator().next();
+        assertEquals(false,mortal_kombat_komplete_edition.getValueById("money"));
+        assertEquals(true,mortal_kombat_komplete_edition.getValueById("graphics"));
+        assertEquals(true,mortal_kombat_komplete_edition.getValueById("multiplayer"));
+        assertEquals(true,mortal_kombat_komplete_edition.getValueById("pegi"));
+        assertEquals(false,mortal_kombat_komplete_edition.getValueById("type"));
+        assertEquals(true,mortal_kombat_komplete_edition.getValueById("view"));
+        assertEquals(false,mortal_kombat_komplete_edition.getValueById("details"));
     }
 }
