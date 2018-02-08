5ef98aee3fcdd44ca035b4bde0852dc645f4e6c5
daviddude <uszkai.d@gmail.com>
2018-02-08 09:21:49 +0100

    evaulateAnswerByInput now using getInputPattern

--- src/main/java/com/codecool/Answer.java
+++ src/main/java/com/codecool/Answer.java
@@ -9,9 +9,9 @@ public class Answer {
 
     public boolean evaluateAnswerByInput(String input) throws Exception {
         if (values.get(0) instanceof SingleValue) {
-            if (input.equals(values.get(0).param.get(0).toString())) {
+            if (input.equals(values.get(0).getInputPattern().get(0).toString())) {
                 return values.get(0).selectionType;
-            } else if (input.equals(values.get(1).param.get(0).toString())) {
+            } else if (input.equals(values.get(1).getInputPattern().get(0).toString())) {
                 return values.get(1).selectionType;
             }
             throw new Exception("Please enter either " + values.get(0).param.get(0) + " or " + values.get(1).param.get(0));
@@ -23,9 +23,9 @@ public class Answer {
                 possibleAnswer += values.get(1).param.get(j)+"\n";
             }
             for (int j = 0; j < values.get(0).param.size(); j++) {
-                if (input.equals(values.get(0).param.get(j).toString())) {
+                if (input.equals(values.get(0).getInputPattern().get(j).toString())) {
                     return values.get(0).selectionType;
-                } else if (input.equals(values.get(1).param.get(j).toString())) {
+                } else if (input.equals(values.get(1).getInputPattern().get(j).toString())) {
                     return values.get(1).selectionType;
                 }
             }
