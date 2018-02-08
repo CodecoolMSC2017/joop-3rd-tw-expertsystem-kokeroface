33a1660b39705aeb5acc7ff6a0700af973ec4172
daviddude <uszkai.d@gmail.com>
2018-02-08 11:06:55 +0100

    RuleRepository private field change to protected

--- src/main/java/com/codecool/RuleRepository.java
+++ src/main/java/com/codecool/RuleRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class RuleRepository {
 
-    private List<Question> questions = new ArrayList<>();
+    protected List<Question> questions = new ArrayList<>();
 
     public void addQuestion(Question question) {
         questions.add(question);
